8776c33692db608acacc5aedd10f0f7cf1731f14
Rohan <2160978@talnet.nl>
2024-10-18 10:03:51 +0200

    logout button gefixt

--- functions.php
+++ functions.php
@@ -2,25 +2,22 @@
 
 function check_login($con)
 {
-
-    if(isset($_SESSION['user_id']))
-    {
-
+    if (isset($_SESSION['user_id'])) {
         $id = $_SESSION['user_id'];
-        $query = "select * from users where user_id = '$id' limit 1";
+        $query = "SELECT * FROM users WHERE user_id = '$id' LIMIT 1";
 
-        $result = mysqli_query($con,$query);
-        if ($result && mysqli_num_rows($result) >0)
-        {
-
+        $result = mysqli_query($con, $query);
+        if ($result && mysqli_num_rows($result) > 0) {
             $user_data = mysqli_fetch_assoc($result);
             return $user_data;
         }
     }
-    // redirect to login
-    header("location: login.php");
+
+    // Stuur de gebruiker naar de login-pagina als hij niet is ingelogd
+    header("Location: login.php");
     die;
 }
+
 
 
 
