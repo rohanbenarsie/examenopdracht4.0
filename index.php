489f5ec3361d8ada96ea78c694a487ac69dfa80f
Rohan <2160978@talnet.nl>
2024-10-18 09:56:44 +0200

    probleem gefixt als ik url typ kom ik in de index.php nu kom ik op de login.php en daarna in de index.html

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
 <?php
-    session_start();
+session_start();
 
-    include("connection.php");
-    include("functions.php");
+include("connection.php");
+include("functions.php");
 
-    $user_data = check_login($con);
+$user_data = check_login($con);
+
+// Controleer of de gebruiker is ingelogd
+if (!isset($_SESSION['user_id'])) {
+    // Gebruiker is niet ingelogd, doorverwijzen naar de signup pagina
+    header("Location: signup.php");
+    exit();
+}
 ?>
-
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>My website</title>
-    </head>
-    <body>
+<head>
+    <title>My website</title>
+</head>
+<body>
 
     <a href="logout.php">Logout</a>
-    <h1>this is the index.php </h1>
+    <h1>Welcome to the index page</h1>
 
     <br>
-    hello <?php echo $user_data['user_name']; ?>
-    </body>
+    Hello, <?php echo $user_data['user_name']; ?>
+</body>
 </html>
